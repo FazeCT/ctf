733bcde1da9b32c7dce61632a24a15fe8c8c0c3c
FazeCT <110371121+FazeCT@users.noreply.github.com>
2022-08-10 19:17:10 +0700

    Create Raspberry.py

---
+++ CTFlearn/Raspberry.py
@@ -0,0 +1,9 @@
+#Made by FazeCT
+
+print('CTFlearn{+Fru',end='')
+n1 = int('0x89d',0)
+n2 = int('0x15',0)
+print(chr(n1//n2),end='')
+n3 = 5 * int('0x15',0) + int('0xb',0)
+print(chr(n3),end='')
+print('123}',end='')
