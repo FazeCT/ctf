30b2333e66ca17a53f95d1e8a59f7ea820c98cbe
FazeCT <110371121+FazeCT@users.noreply.github.com>
2022-08-15 01:52:00 +0700

    Create droids2.py

---
+++ picoCTF/droids2.py
@@ -0,0 +1,9 @@
+#Made by FazeCT
+
+witches = ["weatherwax", "ogg", "garlick", "nitt", "aching", "dismass"]
+second = 3 - 3
+third = (3 // 3) + second
+fourth = (third + third) - second
+fifth = 3 + fourth
+key = witches[fifth] + "." + witches[third] + "." + witches[second] + "." + witches[(fifth + second) - third] + "." + witches[3] + "." + witches[fourth]
+print(key)
