ab8c160867ea0efb10bd2dd0091bf9b63f182fc5
FazeCT <110371121+FazeCT@users.noreply.github.com>
2025-05-04 21:53:05 +0700

    Update objdump_patch.c

--- objdump/objdump_patch.c
+++ objdump/objdump_patch.c
@@ -16,15 +16,13 @@ struct display_target {
 const char ref_0x231930_gnu_binutils_version[];
 const char ref_0x231950_bfd_version[];
 
-int ref_0x5FEC0_printf(const char *format, ...) {};
-void ref_0x60400_free(void *ptr) {};
 void ref_0xB7AB7_display_target_list(struct display_target *arg) {};
 void ref_0xB7CEB_display_target_tables(const struct display_target *arg) {};
 
 int fix_0xB7E74_display_info() {
     struct display_target arg;
 
-    ref_0x5FEC0_printf(ref_0x231950_bfd_version, ref_0x231930_gnu_binutils_version);
+    printf(ref_0x231950_bfd_version, ref_0x231930_gnu_binutils_version);
 
     ref_0xB7AB7_display_target_list(&arg);
     if (!arg.error)
